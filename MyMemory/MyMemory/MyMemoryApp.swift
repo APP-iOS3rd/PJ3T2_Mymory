91ecde437bb27b1fa518759663909936b6dfc68a
iAmSomething <st939823@gmail.com>
2024-01-02 16:17:00 +0900

    :tada: 프로젝트 시작

---
+++ MyMemory/MyMemory/MyMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemoryApp.swift
+//  MyMemory
+//
+//  Created by 김태훈 on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
