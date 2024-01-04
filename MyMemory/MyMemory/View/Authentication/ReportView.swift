634190f85a1edc82378aa0a4425b10560ffed3a1
xohxe <saeyan2477@gmail.com>
2024-01-04 15:35:06 +0900

    Feat: Report View(신고하기페이지) 구현

--- MyMemory/MyMemory/View/Authentication/ReportView.swift
+++ MyMemory/MyMemory/View/Authentication/ReportView.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 struct ReportView: View {
     var body: some View {
+        
         NavigationView {
-            VStack{
-                Textarea(label:"메모 제목")
-                Textarea(label:"어떤 문제가 있나요?",placeholder: "사회/정치적으로 부적절한 메시지가 있어요.")
-                Textarea(label:"자세히 설명해주세요")
+            ZStack {
+                VStack{
+                    Textarea(label:"메모 제목")
+                    Textarea(label:"어떤 문제가 있나요?",placeholder: "사회/정치적으로 부적절한 메시지가 있어요.")
+                    Textarea(label:"자세히 설명해주세요")
+                    Spacer()
+                }
+            }
+            .navigationTitle("신고하기")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("이전"){
+                        
+                    }
+                    
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("등록하기"){
+                        
+                    }
+                }
             }
         }
-        .navigationTitle("신고하기")
+     
+        
     }
 }
 
