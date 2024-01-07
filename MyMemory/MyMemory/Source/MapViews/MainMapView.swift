df258709b5384f3ec52bcd0dc5a00023b9e78c30
iAmSomething <st939823@gmail.com>
2024-01-07 15:41:34 +0900

    :triangular_flag_on_post: Map뷰 추가

---
+++ MyMemory/MyMemory/Source/MapViews/MainMapView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMapView.swift
+//  MyMemory
+//
+//  Created by 김태훈 on 1/2/24.
+//
+
+import SwiftUI
+import MapKit
+struct MainMapView: View {
+    var body: some View {
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    MainMapView()
+}
---
+++ MyMemory/MyMemory/Source/MapViews/MainMapViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainMapViewModel.swift
+//  MyMemory
+//
+//  Created by 김태훈 on 1/2/24.
+//
+
+import Foundation
+import Combine
+import MapKit
+final class MainMapViewModel: ObservableObject {
+}
