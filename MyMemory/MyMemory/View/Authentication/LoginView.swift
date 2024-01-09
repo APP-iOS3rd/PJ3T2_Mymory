d4241600e46af3589f3087f13a2466ce3ee0cb2c
RapidSloth <yud1230@naver.com>
2024-01-09 21:39:41 +0900

    [Feat]: 로그인 뷰 - UI 임시 구현했습니다 - 로그인 관련 로직 일부 구현 1. 엔터 입력시 텍스트 필드 자동 전환 2. 화면 터치 >> 키보드 숨기기 3. 아이디나 비밀번호 둘 중 하나라도 미 입력시 로그인 버튼 비활성화
    - 논의해야 하는 것들
    1. 소셜 로그인 버튼 >> 몇개를 지원해야 할지
    2. 둘러보기 버튼 >> 앱 시작화면을 지도로 할지 로그인 뷰로 할지. 지도로 시작하게 된다면 필요없는 기능

--- MyMemory/MyMemory/View/Authentication/LoginView.swift
+++ MyMemory/MyMemory/View/Authentication/LoginView.swift
@@ -7,11 +7,128 @@
 
 import SwiftUI
 
-struct LoginView: View {
-    var body: some View {
-        Text("로그인뷰")
+// 화면 터치 시 키보드 숨기기
+extension View {
+    func endTextEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+
+struct LoginView: View {
+    
+    // 엔터 >> 자동으로 다음 텍스트필드로 이동
+    enum Field {
+        case email
+        case password
+    }
+    @FocusState private var focusedField: Field?
+    
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+
+    @State private var isActive: Bool = false
+    @State private var notCorrectLogin: Bool = false
+    
+    
+    private var correctEmail: String = "12345@naver.com"
+    private var correctPassword: String = "12345"
+    
+
+    
+    var body: some View {
+        NavigationStack {
+        
+            VStack {
+                
+                Text("내모리")
+                    .font(.system(size: 50, weight: .black))
+                    .foregroundStyle(.indigo)
+                    .padding(.top, 100)
+                    .padding(.bottom, 50)
+                
+                VStack(alignment: .leading) {
+                    Text("이메일")
+                        .foregroundStyle(.gray)
+                    
+                    TextField("", text: $email)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)// 대문자x
+                        .focused($focusedField, equals: .email)
+                        .textContentType(.emailAddress)
+                    
+                    
+                    Text("비밀번호")
+                        .foregroundStyle(.gray)
+                    
+                    SecureField("", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
+                        .focused($focusedField, equals: .password)
+                        .textContentType(.password)
+    
+                }//:VSTACK - TextField
+                .onSubmit {
+                    switch focusedField {
+                    case .email:
+                        focusedField = .password
+                    default:
+                        print("Done")
+                    }
+                }
+                
+                
+                Button(action: {
+                    checkLogin(isEmail: email, isPassword: password)
+                    self.isActive = true
+                }, label: {
+                    Text("로그인")
+                })
+                .padding(.top, 30)
+                .alert(isPresented: $notCorrectLogin) {
+                    Alert(title: Text("주의\n"), message: Text("이메일, 또는 비밀번호가 일치하지 않습니다."), dismissButton: .default(Text("확인")))
+                }
+                .disabled(self.email.isEmpty || self.password.isEmpty)
+                
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    Text("내모리가 처음이라면 - 회원가입")
+                        .underline()
+                        .foregroundStyle(.gray)
+                        .font(.system(size: 17))
+                        .padding(.top, 1)
+                }
+
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "apple.logo")
+                    Text("애플로 로그인 하기")
+                })
+                .padding()
+            }//: VSTACK
+            .padding()
+            .navigationDestination(isPresented: $isActive) {
+                EmptyView()
+            }
+        }//: Navigation Stack
+        .onTapGesture{
+            self.endTextEditing()
+        }
+    }
+    
+    
+    private func checkLogin(isEmail: String, isPassword: String) {
+        if isEmail != correctEmail || isPassword != correctPassword {
+            notCorrectLogin = true
+        }
+    }
+    
+}
+
 
 #Preview {
     LoginView()
