79bfe55d978ad66e6bd69514d0e6f8f6dcc65989
Ahario <81345604+Ahario@users.noreply.github.com>
2024-01-10 10:08:20 +0900

    [Add] RegisterView 추가 (#12)

---
+++ MyMemory/MyMemory/View/Authentication/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  MyMemory
+//
+//  Created by hyunseo on 1/8/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
