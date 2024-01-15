1bc348143aac8136b02b7e69f9415a9b9c2c447d
xohxe <saeyan2477@gmail.com>
2024-01-15 23:57:01 +0900

    Feat: Extension (Color, Font, Button)
    - Color+Extension
      - assets 대신, extension에서 컬러 값을 바꿀 수 있게 코드를 작성했어요.
      - RGB 값 대신 HEX 값으로 변환해주는 코드를 작성해서, 간편하게 색상을 지정할 수 있어요.
    - Font+Extension
      - 파일명을 변경하고, 순서를 오름차순으로 정렬했어요.
      - info.plist의 순서를 굵기순으로 정렬했어요.
    - Button+Extension
      - Pill, RoundedRect 두 가지 커스텀 형태의 버튼 익스텐션을 작성했어요.

---
+++ MyMemory/MyMemory/Resource/Extension/Button+Extension.swift
@@ -0,0 +1,75 @@
+//
+//  Button+Extension.swift
+//  MyMemory
+//
+//  Created by 김소혜 on 1/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+// Pill(알약형태)
+// 다음과 같이 사용해주세요. .buttonStyle(Pill.standard)
+struct Pill: ButtonStyle {
+    let backgroundColor: Color
+    let titleColor: Color
+    let setFont: Font
+    var paddingVertical: CGFloat = 12
+    var paddingHorzontal: CGFloat = 16
+    
+    static var standard: Pill {
+        return Pill(backgroundColor: .accentColor, titleColor: .white, setFont: .bold16)
+    }
+ 
+    static var lightGray: Pill {
+        return Pill(backgroundColor: .lightGray, titleColor: .darkGray, setFont: .bold12, paddingVertical: 8, paddingHorzontal: 12)
+    }
+    
+    static var deepGray: Pill {
+        return Pill(backgroundColor: .deepGray, titleColor: .white, setFont: .bold12, paddingVertical: 8, paddingHorzontal: 12)
+    }
+    static var secondary: Pill {
+        return Pill(backgroundColor: .deepGray, titleColor: .white, setFont: .bold14)
+    }
+    
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(setFont)
+            .padding(.horizontal, paddingHorzontal)
+            .padding(.vertical, paddingVertical)
+            .background(backgroundColor)
+            .foregroundColor(titleColor)
+            .clipShape(Capsule())
+    }
+}
+
+
+struct RoundedRect: ButtonStyle {
+    let backgroundColor: Color
+    let titleColor: Color
+    let setFont: Font
+    let paddingVertical: CGFloat = 12
+    let paddingHorzontal: CGFloat = 16
+    let cornerRadius: CGFloat = 10
+    
+    static var standard: RoundedRect {
+        return RoundedRect(backgroundColor: .white, titleColor: .darkGray, setFont: .bold14)
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(setFont)
+            .padding(.horizontal, paddingHorzontal)
+            .padding(.vertical, paddingVertical)
+            .foregroundColor(titleColor)
+            .background(backgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+            .overlay(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                .stroke(Color(UIColor.systemGray4))
+            )
+    }
+}
+
