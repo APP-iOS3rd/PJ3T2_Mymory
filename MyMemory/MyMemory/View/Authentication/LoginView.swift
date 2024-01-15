2607cfb2b2662dbc00781e8acb2b754c32a3664f
Sungyeop Kim <144765545+RapidSloth@users.noreply.github.com>
2024-01-15 18:09:34 +0900

    Feat/login view (#30)
    * [Feat]: 로그인 뷰
    - UI 임시 구현했습니다
    - 로그인 관련 로직 일부 구현
    1. 엔터 입력시 텍스트 필드 자동 전환
    2. 화면 터치 >> 키보드 숨기기
    3. 아이디나 비밀번호 둘 중 하나라도 미 입력시 로그인 버튼 비활성화
    
    - 논의해야 하는 것들
    1. 소셜 로그인 버튼 >> 몇개를 지원해야 할지
    2. 둘러보기 버튼 >> 앱 시작화면을 지도로 할지 로그인 뷰로 할지. 지도로 시작하게 된다면 필요없는 기능
    
    * [Feat]: 로그인 버튼
    - 로그인 버튼 활성화 및 비활성화
    - 기본 >> disabled된 회색버튼
    - 아이디와 비밀번호 입력시 활성화된 버튼으로 변경
    
    * [Feat]: 커스텀 버튼스타일 추가
    - 로그인 버튼과 텍스트필드 위치 조정
    - 로그인 버튼, SNS로그인 버튼 >> 커스텀으로 구현
    
    * [Feat]: 커스텀 버튼 수정
    
    * [Feat]: 화면 연결
    - 임시 아이디로 로그인 >> MapView 이동
    - 회원가입 버튼과 화면 연결
    
    * [Feat]: 로그인 화면 UI
    - 커스텀 텍스트필드 적용
    - 내모리 임시 로고 삽입
    - 버튼 사이즈 조정
    - 카카오 로그인 버튼 추가
    
    * [Feat]: 로그인 화면 UI
    - 불필요한 기능 주석처리
    
    * 최신화
    
    ---------
    
    Co-authored-by: 김태훈 <38745420+iAmSomething@users.noreply.github.com>

--- MyMemory/MyMemory/View/Authentication/LoginView.swift
+++ MyMemory/MyMemory/View/Authentication/LoginView.swift
@@ -6,11 +6,172 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+// 화면 터치 시 키보드 숨기기
+extension View {
+    func endTextEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
 
 struct LoginView: View {
-    var body: some View {
-        Text("로그인뷰")
+    
+    // 엔터 >> 자동으로 다음 텍스트필드로 이동
+    enum Field {
+        case email
+        case password
     }
+    @FocusState private var focusedField: Field?
+    
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    @State private var isActive: Bool = false
+    @State private var notCorrectLogin: Bool = false
+    
+    // 확인용 임시 아이디 + 패스워드
+//    private var correctEmail: String = "12345@naver.com"
+//    private var correctPassword: String = "12345"
+    
+
+    
+    var body: some View {
+        NavigationStack {
+        
+            VStack {
+                
+                
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .padding(.vertical, 50)
+                
+                
+                VStack(spacing: 20) {
+                    VStack(alignment: .leading) {
+                        Text("이메일")
+                            .foregroundStyle(.gray)
+                            .font(.regular14)
+                        
+                        TextField("", text: $email)
+                            .textFieldStyle(UnderLineTextfieldStyle())
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.never)// 대문자x
+                            .focused($focusedField, equals: .email)
+                            .textContentType(.emailAddress)
+                        
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("비밀번호")
+                            .foregroundStyle(.gray)
+                            .font(.regular14)
+                        
+                        SecureField("", text: $password)
+                            .textFieldStyle(UnderLineTextfieldStyle())
+                            .textInputAutocapitalization(.never)
+                            .focused($focusedField, equals: .password)
+                            .textContentType(.password)
+                        
+                    }
+                }//:VSTACK - TextField
+                .onSubmit {
+                    switch focusedField {
+                    case .email:
+                        focusedField = .password
+                    default:
+                        print("Done")
+                    }
+                }
+                // 텍스트필드에 clear버튼 활성화
+                .onAppear {
+                    UITextField.appearance().clearButtonMode = .whileEditing
+                }
+                
+                if self.email.isEmpty || self.password.isEmpty {
+                    Button {
+                        
+                    } label: {
+                        Text("로그인")
+                            .font(.regular18)
+                    }
+                        .buttonStyle(LoginButton())
+                } else {
+                    Button {
+//                        checkLogin(isEmail: email, isPassword: password)
+                        self.isActive = true
+                    } label: {
+                        Text("로그인")
+                            .font(.regular18)
+                    }
+                    .buttonStyle(LoginButton(backgroundColor: Color.indigo))
+//                    .alert(isPresented: $notCorrectLogin) {
+//                        Alert(title: Text("주의\n"), message: Text("이메일, 또는 비밀번호가 일치하지 않습니다."), dismissButton: .default(Text("확인")))
+//                    }
+                }
+    
+                NavigationLink {
+                    RegisterView()
+                } label: {
+                    Text("내모리가 처음이시라면 - 회원가입")
+                        .underline()
+                        .foregroundStyle(.gray)
+                        .font(.regular14)
+                }
+                
+                Spacer()
+                
+                // MARK: - 소셜 로그인 버튼
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "apple.logo")
+                                .resizable()
+                                .frame(width: 18, height: 20)
+                            Text("Apple로 계속하기")
+                                .font(.regular16)
+                        }
+                    }
+                    .buttonStyle(SocialLoginButton())
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image("kakao")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 18, height: 20)
+                            Text("Kakao로 계속하기")
+                                .font(.regular16)
+                        }
+                    }
+                    .buttonStyle(SocialLoginButton(labelColor: Color.black ,backgroundColor: Color.yellow))
+                }//: SNS 로그인
+                .padding(.vertical, 20)
+            }//: VSTACK
+            .padding()
+            .navigationDestination(isPresented: $isActive) {
+                MainMapView()
+            }
+        }//: Navigation Stack
+        .onTapGesture{
+            self.endTextEditing()
+        }
+    }
+    
+    
+//    private func checkLogin(isEmail: String, isPassword: String) {
+//        if isEmail != correctEmail || isPassword != correctPassword {
+//            notCorrectLogin = true
+//        }
+//    }
+    
 }
 
 #Preview {
