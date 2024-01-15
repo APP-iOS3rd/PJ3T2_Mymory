ac5bb1db0d52a8b74a58c86c7a078413af77d22e
Seobe95 <woodyms@nate.com>
2024-01-16 01:10:19 +0900

    [Feat]: Color Extension 추가 - Color(hexString: "FFFFFF")와 같이 사용하시면 됩니다.

--- MyMemory/MyMemory/Resource/Color+Extensions.swift
+++ MyMemory/MyMemory/Resource/Color+Extensions.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+// MARK: hex코드를 RGB로 변환하는 extension. ex) Color(hexString: "FFFFFF")
+extension Color {
+    init(hexString: String, opacity: Double = 1.0) {
+        let hex: Int = Int(hexString, radix: 16)!
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+        
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
+
