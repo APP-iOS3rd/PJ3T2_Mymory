067a3344ef9aa1dd60a154169f0c519e4a6a7303
Seobe95 <woodyms@nate.com>
2024-01-16 01:14:47 +0900

    [Refactoring]: 최신 피그마 디자인에 맞는 화면으로 리팩토링하였습니다. - MemoListCell 내 해당 장소 메모보기 라는 이름의 버튼의 크기가 너무 작은 것은 아닌지 의논이 필요할 것 같습니다.

--- MyMemory/MyMemory/View/Authentication/Mypage/MemoListCell.swift
+++ MyMemory/MyMemory/View/Authentication/Mypage/MemoListCell.swift
@@ -15,30 +15,58 @@ struct MemoListCell: View {
     
     var body: some View {
         HStack {
-            VStack(alignment: .leading, spacing: 3) {
-                Text(title)
-                    .font(.bold14)
-                if let tags = tags, !tags.isEmpty {
-                    HStack {
-                        ForEach(tags, id: \.self) { tag in
-                            Text("#\(tag)")
-                                .font(.semibold11)
+            HStack(alignment: .top, spacing: 10){
+                Circle()
+                    .frame(width: 42, height: 42)
+                    .foregroundStyle(Color(hexString: "F5F5F5"))
+                    .overlay(alignment: .center) {
+                        Image(systemName: "heart.fill")
+                            .font(.system(size: 16))
+                            .foregroundStyle(Color(hexString: "EF8E8E"))
+                    }
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    if let tags = tags, !tags.isEmpty {
+                        HStack(spacing: 2) {
+                            ForEach(tags, id: \.self) { tag in
+                                Text("#\(tag)")
+                                    .font(.regular12)
+                                    .foregroundStyle(Color(hexString: "898A8D"))
+                            }
                         }
                     }
-                }
-                HStack {
-                    Text(date)
-                        .padding(.trailing, 4)
-                        .font(.regular11)
-                    Text("﹒\(address)")
-                        .font(.regular11)
+                    Text(title)
+                        .font(.bold16)
+                    
+                    Text("해당 장소 메모보기")
+                        .padding(EdgeInsets(top: 6, leading: 13, bottom: 6, trailing: 13))
+                        .background(Color(hexString: "F4F4F4"))
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .foregroundStyle(Color(hexString: "5a5a5a"))
+                        .font(.extraBold12)
+                    
+                    HStack(spacing: 2) {
+                        Image(systemName: "heart.fill")
+                            .font(.system(size: 11))
+                        Text("0개")
+                            .font(.regular12)
+                        Text("|")
+                            .font(.semibold11)
+                        Image(systemName: "location.fill")
+                            .font(.system(size: 11))
+                        Text("38m")
+                            .font(.regular12)
+                    }
+                    .foregroundStyle(Color(hexString: "898A8D"))
+                    .padding(.top, 27)
                 }
             }
             
             Spacer()
         }
         .padding(EdgeInsets(top: 12, leading: 18, bottom:12, trailing: 18))
-        .background(Color(UIColor.systemGray6))
-        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
     }
 }
+
