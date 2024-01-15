8888644cdff5c7e1ffc81cb3b01155e5f1d5d0cc
Seobe95 <woodyms@nate.com>
2024-01-15 09:53:00 +0900

    [Feat]: 설정화면 구현 - 각 세부 설정화면은 구현이 필요합니다.

--- MyMemory/MyMemory/View/Setting/SettingMenuCell.swift
+++ MyMemory/MyMemory/View/Setting/SettingMenuCell.swift
@@ -2,17 +2,31 @@
 //  SettingMenuCell.swift
 //  MyMemory
 //
-//  Created by 이명섭 on 1/12/24.
+//  Created by 이명섭 on 1/8/24.
 //
 
 import SwiftUI
 
 struct SettingMenuCell: View {
+    var name: String
+    var iconName: String?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink {
+            
+        } label: {
+            HStack(alignment: .center) {
+                Text(name)
+                    .font(.regular18)
+                Spacer()
+                if let iconName {
+                    Image(systemName: iconName)
+                        .font(.system(size: 24))
+                } else {
+                    Text("1.0.0")
+                        .foregroundStyle(Color(UIColor.systemGray))
+                }
+            }.foregroundStyle(.black)
+        }
     }
 }
 
-#Preview {
-    SettingMenuCell()
-}
