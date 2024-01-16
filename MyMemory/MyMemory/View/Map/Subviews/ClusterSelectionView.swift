3ad9e0e5498dd764c80566f9dec69de85f87c2ce
iAmSomething <st939823@gmail.com>
2024-01-16 11:09:03 +0900

    :sparkles: 주소 찾기 기능 주소 찾기 기능을 추가했습니다.
    Shared라는 그룹 내부에 GetAddress.swift를 추가했습니다.
    ```swift
    let address: String = GetAddress.shared.getUserLocation(location: .init(longitude: Double, latitude: Double)
    ```
    이런 식으로 사용하시면 됩니다.

--- MyMemory/MyMemory/View/Map/Subviews/ClusterSelectionView.swift
+++ MyMemory/MyMemory/View/Map/Subviews/ClusterSelectionView.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 struct ClusterSelectionView: View {
     @EnvironmentObject var viewModel: MainMapViewModel
+<<<<<<< HEAD
     let contents: [MiniMemoModel]
     @Binding var selectedItemID: UUID
     var body: some View {
@@ -65,6 +66,40 @@ struct ClusterSelectionView: View {
                             }
                             
                         }
+=======
+    let contents: MemoCluster
+    var body: some View {
+        VStack {
+
+            ScrollView(.horizontal) {
+                HStack{
+                    ForEach(contents.memos) { item in
+                        VStack {
+                            HStack {
+                                Text("\(item.title)")
+                                    .lineLimit(1)
+                                    .font(.bold20)
+                                Spacer()
+                                Text("\(item.createdAt.createdAtTimeYYMMDD)")
+                                    .font(.regular12)
+                                    .foregroundStyle(Color.gray)
+                            }
+                            .padding([.leading, .trailing], 14)
+                            Text(item.contents)
+                                .lineLimit(3)
+                                .font(.regular14)
+                                .frame(maxWidth: .infinity)
+                                .padding([.leading,.trailing], 14)
+                                .padding(.bottom, 20)
+                                .padding(.top, 10)
+                        
+                        }.frame(width: 300)
+                        .padding(.top, 10)
+                        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+                            .cornerRadius(15)
+                            .padding(10)
+                            
+>>>>>>> d71efe2 (:sparkles: 주소 찾기 기능)
                     }
                 }
             }
@@ -72,5 +107,9 @@ struct ClusterSelectionView: View {
         }
         .frame(maxWidth: .infinity)
         .background(Color.clear)
+<<<<<<< HEAD
+=======
+        .clipShape(RoundedCornersShape(radius: 20,corners: [.topLeft,.topRight]))
+>>>>>>> d71efe2 (:sparkles: 주소 찾기 기능)
     }
 }
