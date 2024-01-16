a0ff80b77bc2ff945988a0224e71822b3cd201e3
iAmSomething <st939823@gmail.com>
2024-01-16 09:26:40 +0900

    :rotating_light: Kakaomap Touch event 안되는 현상 돌려막기 Issue : Kakaomap에서 줌이나 틸트는 되는데 터치이벤트만 안먹는 상황입니다.
    해결 아이디어
    touchesBegan()함수는 작동한다. 그러니 UITouch에서 touch locaiton과 radius를 토대로
    맵 CLLocation을 찾아서 그 원형 안에 poi(annotation)이 있으면 그걸 리턴하면 touch event psudo 구현이 될 것 같다.
    
    해결
    poi를 touch할 수 있게 되었습니다.

--- MyMemory/MyMemory/View/Map/KakaoMap.swift
+++ MyMemory/MyMemory/View/Map/KakaoMap.swift
@@ -24,6 +24,7 @@ struct KakaoMapView: UIViewRepresentable {
         view.setDelegate(context.coordinator)
         view.sizeToFit()
         view.setDelegate(context.coordinator)
+        view.sizeToFit()
         context.coordinator.createController(view)
         context.coordinator.controller?.initEngine()
         return view
@@ -174,7 +175,6 @@ struct KakaoMapView: UIViewRepresentable {
         }
         func resetLocation(latitude: Double?, longitude: Double?) {
             if let mapView: KakaoMap = controller?.getView("mapview") as? KakaoMap {
-            
                 let cameraUpdate: CameraUpdate = CameraUpdate.make(target: MapPoint(longitude: longitude ??  127.108678, latitude: latitude ?? 37.402001), zoomLevel: 16, mapView: mapView)
                 mapView.animateCamera(cameraUpdate: cameraUpdate, options: .init())
                 let trackingManager = mapView.getTrackingManager()
@@ -208,8 +208,6 @@ struct KakaoMapView: UIViewRepresentable {
                     tempPoi?.addBadge(badge)
                     tempPoi?.show()
                     tempPoi?.showBadge(badgeID: "\(c.id)")
-
-
                 }
             }
         }
