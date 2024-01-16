0eaa24768a3654a8356a2dfcf3625a538fc2a049
Seobe95 <woodyms@nate.com>
2024-01-16 17:00:45 +0900

    [Feat]: 폰트 크기 추가

---
+++ MyMemory/MyMemory/View/Authentication/Mypage/MypageMemoList.swift
@@ -0,0 +1,18 @@
+//
+//  MypageMemoList.swift
+//  MyMemory
+//
+//  Created by 이명섭 on 1/16/24.
+//
+
+import SwiftUI
+
+struct MypageMemoList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MypageMemoList()
+}
---
+++ MyMemory/MyMemory/View/Setting/SettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewModel.swift
+//  MyMemory
+//
+//  Created by 이명섭 on 1/16/24.
+//
+
+import Foundation
