3c4cb269c505e5b4d7c44587b15171a2ced926c8
jeonguk29 <jeonguk29@naver.com>
2024-01-18 13:38:28 +0900

    [Refactor] 메모 이미지 압축 코드 추가

--- MyMemory/MyMemory/API/MemoService.swift
+++ MyMemory/MyMemory/API/MemoService.swift
@@ -16,30 +16,40 @@ struct MemoService {
     let storage = Storage.storage()
     
     // 이미지를 업로드하고 URL을 반환하는 함수
-    private func uploadImage(imageData: Data) async throws -> String {
+    private func uploadImage(originalImageData: Data) async throws -> String {
+        // Data에서 UIImage를 생성
+        guard let image = UIImage(data: originalImageData) else {
+            throw NSError(domain: "Invalid image data", code: 0, userInfo: nil)
+        }
+
+        // UIImage를 압축하여 새로운 Data 객체를 생성
+        guard let compressedImageData = image.jpegData(compressionQuality: 0.75) else {
+            throw NSError(domain: "Image compression failed.", code: 0, userInfo: nil)
+        }
+
         let storageRef = storage.reference()
         let imageRef = storageRef.child("images/\(UUID().uuidString).jpg")
         let metadata = StorageMetadata()
         metadata.contentType = "image/jpeg"
         
-        // 이미지 업로드
-        let _ = try await imageRef.putData(imageData, metadata: metadata)
+        // 압축된 이미지 데이터를 업로드
+        let _ = try await imageRef.putData(compressedImageData, metadata: metadata)
         
-        // 업로드된 이미지의 URL 가져오기
-        // 업로드된 이미지의 URL 가져오기를 몇 번 시도
-          var downloadURL: URL
-          for _ in 1...5 {
-              do {
-                  downloadURL = try await imageRef.downloadURL()
-                  print("Image uploaded with URL: \(downloadURL.absoluteString)")
-                  return downloadURL.absoluteString
-              } catch {
-                  print("Retrying to get download URL...")
-                  try await Task.sleep(nanoseconds: 1_000_000_000) // 2초 대기
-              }
-          }
-          throw URLError(.cannotFindHost) // 적절한 에러 처리 필요
+        // 업로드된 이미지의 URL 가져오기를 몇 번 시도 (처리가 좀 늦는 경우를 대비)
+        var downloadURL: URL
+        for _ in 1...3 {
+            do {
+                downloadURL = try await imageRef.downloadURL()
+                print("Image uploaded with URL: \(downloadURL.absoluteString)")
+                return downloadURL.absoluteString
+            } catch {
+                print("Retrying to get download URL...")
+                try await Task.sleep(nanoseconds: 1_000_000_000) // 1초 대기
+            }
+        }
+        throw URLError(.cannotFindHost) // 적절한 에러 처리 필요
     }
+
     
     // Memo 모델을 넘기자
     func uploadMemo(newMemo: PostMemoModel) async {
@@ -48,7 +58,7 @@ struct MemoService {
         // 이미지 데이터 배열을 반복하면서 각 이미지를 업로드하고 URL을 저장
           for imageData in newMemo.memoSelectedImageData {
               do {
-                  let imageUrl = try await uploadImage(imageData: imageData)
+                  let imageUrl = try await uploadImage(originalImageData: imageData)
                   imageDownloadURLs.append(imageUrl)
                   print("Image URL added: \(imageUrl)")
               } catch {
@@ -76,4 +86,6 @@ struct MemoService {
             print("Error adding document: \(error)")
         }
     }
+    
+
 }
