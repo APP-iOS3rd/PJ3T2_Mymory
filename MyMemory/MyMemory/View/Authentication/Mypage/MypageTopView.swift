cc2f10d2c1cbc82b86644b9fc2f5cdc9879f2405
Seobe95 <woodyms@nate.com>
2024-01-18 11:53:28 +0900

    [Feat]: 로그인 상태에 따른 분기처리 & 로그아웃 및 회원탈퇴 시 Alert처리 - 로그인 상태에 따라 화면의 분기를 처리하였습니다. - 기존의 마이페이지의 프로필 사진과 닉네임, 그리고 작성한 메모는 로그인을 해야 확인 할 수 있도록 처리하였습니다. (추가적인 디자인이 필요할듯 합니다.) - 로직 처리에 따라 코드가 비대해져서, MypageTopView를 생성하여 컴포넌트화하였습니다. - 로그아웃과 회원탈퇴에 성공할 경우 Alert가 나타나도록 처리하였습니다.

--- MyMemory/MyMemory/View/Authentication/Mypage/MypageTopView.swift
+++ MyMemory/MyMemory/View/Authentication/Mypage/MypageTopView.swift
@@ -6,13 +6,58 @@
 //
 
 import SwiftUI
-
+// 마이페이지 최상단의 프로필 및 닉네임 등을 표시하는 View입니다.
 struct MypageTopView: View {
+    @EnvironmentObject var viewModel: MypageViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if viewModel.isCurrentUserLoginState {
+                NavigationLink {
+                    ProfileEditView(
+                        profileImage: $viewModel.selectedImage,
+                        selectedPhotoData: $viewModel.selectedPhotoData
+                    )
+                } label: {
+                    if let data = viewModel.selectedPhotoData, let profileImage = UIImage(data: data) {
+                        Image(uiImage: profileImage)
+                            .resizable()
+                            .scaledToFill()
+                            .clipped()
+                            .clipShape(.circle)
+                            .frame(width: 76, height: 76)
+                    } else {
+                        Circle()
+                            .frame(width: 76, height: 76)
+                            .foregroundStyle(Color(hex: "d9d9d9"))
+                    }
+                    
+                    Text("닉네임")
+                        .font(.semibold20)
+                        .padding(.leading, 10)
+                }
+                .buttonStyle(.plain)
+            } else {
+                NavigationLink {
+                    LoginView()
+                } label: {
+                    Text("로그인이 필요합니다.")
+                        .font(.semibold20)
+                }
+                .buttonStyle(.plain)
+            }
+            
+            Spacer()
+            
+            NavigationLink {
+                SettingView()
+            } label: {
+                Image(systemName: "gearshape")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding(.top, 30)
+        
     }
-}
-
-#Preview {
-    MypageTopView()
 }
