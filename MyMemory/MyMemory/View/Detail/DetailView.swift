560fbe040e3a9e552dca10a7d2a9b5e8d5ec79ac
Sungyeop Kim <144765545+RapidSloth@users.noreply.github.com>
2024-01-23 20:28:28 +0900

    [Feat]: 거리에 따라 디테일뷰 분기 (#92)

---
+++ MyMemory/MyMemory/View/Detail/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  MyMemory
+//
+//  Created by 김성엽 on 1/23/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Binding var memo: Memo
+    @Binding var isVisble: Bool
+    
+    var body: some View {
+        if isVisble {
+            MemoDetailView(memo: memo)
+        } else {
+            CertificationView(memo: $memo)
+        }
+    }
+}
