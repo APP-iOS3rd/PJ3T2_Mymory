0493a25020f0d3341de827735ba58200c1a5cc9d
xohxe.__ <75136643+xohxe@users.noreply.github.com>
2024-01-24 15:57:02 +0900

    Fix: AuthViewModel 내 FetchUser() 무한 호출 이슈

--- MyMemory/MyMemory/View/Main/MainTabView.swift
+++ MyMemory/MyMemory/View/Main/MainTabView.swift
@@ -12,7 +12,7 @@ struct MainTabView: View {
     
 //    @ObservedObject var viewRouter: ViewRouter
     @State private var selectedIndex = 0
-    @EnvironmentObject var viewModel : AuthViewModel
+   // @EnvironmentObject var viewModel : AuthViewModel
     @State var isPresented: Bool = false
  
     @State var isLogin: Bool = false
@@ -49,56 +49,7 @@ struct MainTabView: View {
                         Text("마이")
                     }
                     .tag(2)
-//                // 로그인한 유저 확인
-//                if viewModel.userSession != nil {
-//                    if let user = viewModel.currentUser {
-//                        
-//                   }
-//                } 
-//                // 로그아웃 상태일 때,
-//                else {
-//                    
-//                    Text("로그인이 필요합니다.")
-//                        .onTapGesture{
-//                            selectedIndex = 2
-//                              isLogin = true
-//                        }
-//                        .tabItem {
-//                            Image(systemName: "person")
-//                            Text("마이")
-//                        }
-//                        .tag(2)
-//                    
-//                }
             }
-            // PostView는 Tab전환 대신 Navigation으로 이동
-//            .onChange(of: selectedIndex) { [selectedIndex] newTab in
-//                if newTab == 1 {
-//                    self.selectedIndex = selectedIndex
-//                    isPresented = true
-//                    isLogin = false
-//                    
-//                }
-//                if newTab == 2 && viewModel.userSession == nil {
-//                    self.selectedIndex = selectedIndex
-//                    isLogin = true
-//                    isPresented = false
-//                    
-//                }
-//            }
-//            // NavigationView - LoginView
-//            .background(
-//                
-//                NavigationLink(
-//                    destination: LoginView(),
-//                    isActive: $isLogin
-//                ) {
-//                    EmptyView()
-//                }
-//                    .hidden()
-//                
-//            )
- 
         }
       
     }
