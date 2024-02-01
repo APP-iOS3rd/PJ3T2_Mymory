20b4d9e89ce50d66bccdcd8e31e28ff5e815fe69
iAmSomething <st939823@gmail.com>
2024-02-01 14:07:45 +0900

    temp

--- MyMemory/MyMemory/Source/Shared/PushNotification.swift
+++ MyMemory/MyMemory/Source/Shared/PushNotification.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 /// Push notification을 관리하는 일종의 delegate 느낌으로 만든 단일 싱글톤입니다.
+
 final class PushNotification: ObservableObject {
     static let shared = PushNotification()
     @Published var memo: Memo? = nil
