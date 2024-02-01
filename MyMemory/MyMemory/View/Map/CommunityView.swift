9e50ccb201c0716df845be78446b93ae0ae454ed
Seobe <79817557+Seobe95@users.noreply.github.com>
2024-02-01 17:33:02 +0900

    [Feat/MenuTabBar]: 메뉴 탭 컴포넌트를 구현했습니다. (#152)
    * [Feat]: 메뉴 탭 바 컴포넌트를 구현했습니다.
    - 자세한 사용방법은 MenuTabBar 내부에 적어놓았습니다.
    
    * [Feat]: 메인 뷰의 리스트 뷰에 MenuTabBar를 적용했습니다.
    - CommunityView를 추가했습니다.
    - MainSectionsView를 추가하여 CommunityView, MemoListView를 통합하여 관리합니다.
    - MemoListView 내 NavigationStack을 MainSectionsView로 옮겨 감싸주었습니다.
    
    * [Setting]: 프로젝트 파일

---
+++ MyMemory/MyMemory/View/Map/CommunityView.swift
@@ -0,0 +1,24 @@
+//
+//  CommunityView.swift
+//  MyMemory
+//
+//  Created by 이명섭 on 2/1/24.
+//
+
+import SwiftUI
+
+struct CommunityView: View {
+    var body: some View {
+        Spacer()
+        HStack {
+            Spacer()
+            Text("CommunityView")
+            Spacer()
+        }
+        Spacer()
+    }
+}
+
+#Preview {
+    CommunityView()
+}
