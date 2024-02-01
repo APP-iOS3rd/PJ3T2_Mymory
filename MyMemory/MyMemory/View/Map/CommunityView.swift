d564cb593e403123e0ea1b98bf0fb104549bd414
Seobe95 <woodyms@nate.com>
2024-02-01 16:48:13 +0900

    [Feat]: 메뉴 탭 바 컴포넌트를 구현했습니다. - 자세한 사용방법은 MenuTabBar 내부에 적어놓았습니다.

---
+++ MyMemory/MyMemory/View/Map/CommunityView.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityView.swift
+//  MyMemory
+//
+//  Created by 이명섭 on 2/1/24.
+//
+
+import SwiftUI
+
+struct CommunityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityView()
+}
---
+++ MyMemory/MyMemory/View/Map/MainSectionsView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyMemory
+//
+//  Created by 이명섭 on 2/1/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
