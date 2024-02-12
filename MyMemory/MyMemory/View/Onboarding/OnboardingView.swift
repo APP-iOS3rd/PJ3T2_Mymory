39832448e4130be1808823bacef958934963b931
RapidSloth <yud1230@naver.com>
2024-02-12 22:08:11 +0900

    [Feat]: 온보딩 - UI 수정 - 이미지 Assets 추가

--- MyMemory/MyMemory/View/Onboarding/OnboardingView.swift
+++ MyMemory/MyMemory/View/Onboarding/OnboardingView.swift
@@ -17,17 +17,19 @@ struct OnboardingView : View {
         TabView(selection: $currentIndex.animation()) { // 1
             ForEach(Array(zip(viewModel.onboardingList.indices, viewModel.onboardingList)), id: \.0) { index, item in
                 ZStack {
-//                    Color.lightGrayBackground
-//                        .ignoresSafeArea()
-                    
-                    VStack(spacing:10){
-                        Text(item.title)
-                            .font(.bold20)
+                    Color.white
+                        .ignoresSafeArea()
+                        
+                    VStack(spacing: 15) {
                         Text(item.content)
-                            .font(.light14)
+                            .font(.bold20)
+                            .multilineTextAlignment(.center)
+                        
+                        Image(item.image)
+                            .resizable()
+                            .frame(width: UIScreen.main.bounds.size.height * 0.4 ,height: UIScreen.main.bounds.size.height * 0.45)
                     }
-                    
-
+                    //.padding(.top, 10)
                 }
               .tag(index)
           }
@@ -37,63 +39,6 @@ struct OnboardingView : View {
         .overlay(IndexView(numberOfPages: viewModel.onboardingList, currentIndex: $currentIndex, isFirstLaunching: $isFirstLaunching))
       }
     }
-//    var body: some View {
-//
-//        TabView(selection: $currentIndex) {
-//            ForEach(viewModel.onboardingList){ index in
-//                
-//                ZStack(alignment: .bottom){
-//                    index.bgColor
-//                        .ignoresSafeArea()
-//                    Image(systemName: "square.and.arrow.up")
-//                    //Image(systemName: index.image)
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 280, height: 280)
-//                    .offset(y: -10)
-//                    
-//                    ZStack(alignment: .bottom) {
-//                        
-//                        Image(systemName: index.image)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 280, height: 280)
-//                            //.offset(y: 0)
-//                        
-//                        VStack(spacing:10){
-//                            Text(index.title)
-//                                .font(.bold20)
-//                            Text(index.content)
-//                                .font(.light14)
-//                            Spacer()
-//                            
-//                            
-//                            Button {
-//                                if currentIndex == 2 {
-//                                    isFirstLaunching.toggle()
-//                                } else {
-//                                    currentIndex = currentIndex + 1
-//                                }
-//                            } label: {
-//                                if currentIndex == 2 {
-//                                    Text("시작하기")
-//                                } else {
-//                                    Text("다음으로")
-//                                }
-//                            }
-//                        }
-//                        .padding(.horizontal,30)
-//                        .padding(.vertical,70)
-//                        .frame(maxWidth: .infinity)
-//                        .frame(height: 260)
-//                        .background(Color.white)
-//                    }
-//                }
-//            }
-//        }
-//        .ignoresSafeArea()
-//        .tabViewStyle(PageTabViewStyle())
-//        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
  
  
 //#Preview {
@@ -103,83 +48,76 @@ struct OnboardingView : View {
 
 
 struct IndexView: View {
-  
-  let numberOfPages: [Onboarding]
-  @Binding var currentIndex: Int
+    
+    let numberOfPages: [Onboarding]
+    @Binding var currentIndex: Int
     @Binding var isFirstLaunching: Bool
-  
-  private let circleSize: CGFloat = 16
-  private let circleSpacing: CGFloat = 12
-  
-  private let primaryColor = Color.white
-  private let secondaryColor = Color.white.opacity(0.6)
-  
-  private let smallScale: CGFloat = 0.6
-  
-  
-  // MARK: - Body
+    
+    private let circleSize: CGFloat = 12
+    private let circleSpacing: CGFloat = 12
+    
+    private let primaryColor = Color.primary
+    private let secondaryColor = Color.lightGray
+    
+    private let smallScale: CGFloat = 0.8
+    
+    
+    // MARK: - Body
     
     var body: some View {
-            VStack {
-                Spacer()
-                ZStack {
-                    
-                    Color.lightGrayBackground
-                        .ignoresSafeArea()
-                        .frame(height: 70)
-                    
-                    HStack {
-                        if currentIndex == 3 {
-                            Button {
-                                isFirstLaunching.toggle()
-                            } label: {
-                                Text("시작하기")
-                            }
-                            
-                        } else {
-                            Button {
-                                isFirstLaunching.toggle()
-                            } label: {
-                                Text("SKIP")
-                            }
-                            
-                            Spacer()
-                            
-                            HStack(spacing: circleSpacing) {
-                                ForEach(numberOfPages.indices, id: \.self) { index in
-                                    if shouldShowIndex(index) {
-                                        Circle()
-                                            .fill(currentIndex == index ? primaryColor : secondaryColor)
-                                            .scaleEffect(currentIndex == index ? 1 : smallScale)
-                                        
-                                            .frame(width: circleSize, height: circleSize)
-                                            .transition(AnyTransition.opacity.combined(with: .scale))
-                                            .id(index)
-                                    }
-                                }
-                            }
-                            Spacer()
-                            Button {
-                                if currentIndex != 3 {
-                                    withAnimation {
-                                        currentIndex = currentIndex + 1
-                                    }
-                                }
-                            } label: {
-                                Text("NEXT")
-                            }
-                        }
-                            
+        VStack {
+            HStack(spacing: circleSpacing) {
+                ForEach(numberOfPages.indices, id: \.self) { index in
+                    if shouldShowIndex(index) {
+                        Circle()
+                            .fill(currentIndex == index ? primaryColor : secondaryColor)
+                            .scaleEffect(currentIndex == index ? 1 : smallScale)
+                        
+                            .frame(width: circleSize, height: circleSize)
+                            .transition(AnyTransition.opacity.combined(with: .scale))
+                            .id(index)
                     }
-                    .padding()
                 }
+            }
+            .padding(.top, 100)
+            
+            Spacer()
+            
+            ZStack {
+                
+                HStack {
+                    if currentIndex == 3 {
+                        Button {
+                            isFirstLaunching.toggle()
+                        } label: {
+                            Text("시작하기")
+                                .font(.medium18)
+                                .frame(width: 200, height: 30)
+                        }
+                        .buttonStyle(Pill.standard)
+                        
+                    } else {
+                        Button {
+                            if currentIndex != 3 {
+                                withAnimation {
+                                    currentIndex = currentIndex + 1
+                                }
+                            }
+                        } label: {
+                            Text("다음")
+                                .font(.medium18)
+                                .frame(width: 200, height: 30)
+                        }
+                        .buttonStyle(Pill.standard)
+                    }
+                    
+                }
+                .padding(.bottom, 30)
+            }
         }
     }
-  
-  
-  // MARK: - Private Methods
     
-  func shouldShowIndex(_ index: Int) -> Bool {
-    ((currentIndex - 1)...(currentIndex + 1)).contains(index)
-  }
+    func shouldShowIndex(_ index: Int) -> Bool {
+        ((currentIndex - 3)...(currentIndex + 3)).contains(index)
+    }
 }
--- MyMemory/MyMemory/ViewModel/Onboarding/OnboardingViewModel.swift
+++ MyMemory/MyMemory/ViewModel/Onboarding/OnboardingViewModel.swift
@@ -20,15 +20,14 @@ extension OnboardingViewModel {
     public func fetchModel(){
         self.onboardingList = [
             // 페이지 1: 앱 소개
-            Onboarding(id:0,image: "person.3.fill", title: "메모", content:"잊혀지지 않는 추억, 지금 이곳에서 기록하세요",bgColor: .blue),
+            Onboarding(id:0,image: "onboarding0", title: "앱 핵심 기능 전달", content:"잊혀지지 않는 추억, \n 지금 이곳에서 기록하세요"),
             // 페이지 2: 쓰기 페이지 안내
-            Onboarding(id:1,image: "note.text.badge.plus", title: "메모 작성", content:"사진, 메모, 감정까지, 당신의 추억을 자유롭게 담아보세요", bgColor: .green),
+            Onboarding(id:1,image: "onboarding1-1", title: "앱 주요 기능 설명 1", content:"사진, 메모, 감정까지 \n 당신의 추억을 자유롭게 담아보세요"),
             // 페이지 3: 내 메모 읽기 기능 안내
-            Onboarding(id:2,image: "eyes", title: "메모", content:"지금 이곳에서 떠오르는 추억, 생생하게 되살려 보세요", bgColor: .blue),
-            // 페이지 4: 커뮤니티 페이지 안내 + 온보딩 완료
-            Onboarding(id:3,image: "eyes", title: "메모", content:"가까운 곳에서 다른 사람들의 추억을 찾아보세요.", bgColor: .red)
+            Onboarding(id:2,image: "onboarding2", title: "앱 주요 기능 설명 2", content:"지금 이곳에서의 추억을 \n 다른 사람들과 함께 공유해 보세요"),
+            Onboarding(id:3,image: "onboarding3", title: "앱 주요 기능 설명 3", content:"떠오르는 추억을 \n 어디서나 생생하게 되살려 보세요"),
         ]
     }
-    
+   
 }
 
