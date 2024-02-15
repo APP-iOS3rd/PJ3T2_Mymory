f7c3cb52943d13c9920cdb445824acba7411040b
Sungyeop Kim <144765545+RapidSloth@users.noreply.github.com>
2024-02-15 21:12:16 +0900

    [Refactoring] 커뮤니티에서 거리 상관없이 메모 확인 (#231)
    * [Feat]: 커뮤니티 MemoCell
    - 거리 상관없이 메모들 확인할 수 있게 변경
    
    * 메모 확인가능 거리 조정

--- MyMemory/MyMemory/Source/Components/Memo/MemoCell.swift
+++ MyMemory/MyMemory/Source/Components/Memo/MemoCell.swift
@@ -19,29 +19,27 @@ struct MemoCell: View {
     @Binding var memos: [Memo]
     @State var likeCount = 0
     @State var isMyMemo: Bool = false
+    @State var isFromCo: Bool = false
     
     var body: some View {
         HStack(spacing: 16) {
             
             VStack{
-                if isVisible || isMyMemo {
-                    Button(action: {
+                if isVisible || isMyMemo || isFromCo {
+                    Button {
                         self.memo.didLike.toggle()
                         Task {
                             await MemoService.shared.likeMemo(memo: memo)
                             await fetchlikeCount()
                         }
-                        //print("\(memo.didLike)")
-                        //memo.didLike.toggle()
-                        //print("\(memo.didLike)")
-                    }) {
+                    } label: {
                         Image(systemName: "heart.fill")
                             .foregroundColor(memo.didLike ? .red : .gray)
                             .frame(width: 46, height: 46)
                             .background(Color.bgColor)
                             .clipShape(Circle())
                     }
-                }else {
+                } else {
                     Image(systemName: "lock")
                     .foregroundColor(.gray)
                     .frame(width: 46, height: 46)
@@ -70,18 +68,24 @@ struct MemoCell: View {
                 .foregroundColor(.gray)
                 .font(.regular14)
                 
-                Text(isVisible || isMyMemo ? memo.title : "거리가 멀어서 볼 수 없어요.")
-                    .lineLimit(1)
-                    .font(.black20)
-                    .foregroundStyle(Color.textColor)
-                
-                Button {
-                    // 메모 정보 확인
-                    // 추후 디테일뷰 연결해서 메모 전달 해주면 될거같음
-                } label: {
-                    Text("\(memo.building ?? "해당 장소 메모보기")")
+                if isFromCo {
+                    Text(memo.title)
+                        .lineLimit(1)
+                        .font(.black20)
+                        .foregroundStyle(Color.textColor)
+                } else {
+                    Text(isVisible || isMyMemo ? memo.title : "거리가 멀어서 볼 수 없어요.")
+                        .lineLimit(1)
+                        .font(.black20)
+                        .foregroundStyle(Color.textColor)
                 }
-                .buttonStyle(Pill.deepGray)
+//                Button {
+//                    // 메모 정보 확인
+//                    // 추후 디테일뷰 연결해서 메모 전달 해주면 될거같음
+//                } label: {
+//                    Text("\(memo.building ?? "해당 장소 메모보기")")
+//                }
+//                .buttonStyle(Pill.deepGray)
                 //.buttonStyle(isDark ? Pill.deepGray : Pill.lightGray)
                 
                 Spacer()
@@ -111,8 +115,11 @@ struct MemoCell: View {
                     
                     
                     NavigationLink { // 버튼이랑 비슷함
-                        DetailView(memo: $memo, isVisble: $isVisible, memos: $memos, selectedMemoIndex: selectedMemoIndex, isMyMemo: isMyMemo)
-                        
+                        if isFromCo {
+                            MemoDetailView(memos: $memos, selectedMemoIndex: selectedMemoIndex, isFromCo: isFromCo)
+                        } else {
+                            DetailView(memo: $memo, isVisble: $isVisible, memos: $memos, selectedMemoIndex: selectedMemoIndex, isMyMemo: isMyMemo)
+                        }
                     } label: {
                         HStack {
                             Image(systemName: "location.fill")
@@ -137,7 +144,7 @@ struct MemoCell: View {
         .cornerRadius(20)
         .onAppear {
             if let distance = location?.coordinate.distance(from: memo.location) {
-                if distance <= 50 {
+                if distance <= 20 {
                     isVisible = true
                 } else {
                     isVisible = false
@@ -152,7 +159,7 @@ struct MemoCell: View {
         }
         .onChange(of: location) { oldValue, newValue in
             if let distance = newValue?.coordinate.distance(from: memo.location) {
-                if distance <= 50 {
+                if distance <= 20 {
                     isVisible = true
                 } else {
                     isVisible = false
@@ -166,13 +173,3 @@ struct MemoCell: View {
     }
 }
 
-
-#Preview {
-    VStack {
-        //        MemoCell(isVisible: true, isDark: true)
-        //        MemoCell(isVisible: true, isDark: false)
-        //        MemoCell(isVisible: false, isDark: true)
-        //        MemoCell(isVisible: false, isDark: false)
-    }
-    
-}
